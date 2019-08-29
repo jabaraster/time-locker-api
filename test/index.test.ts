c0d446a4c1971e8fa2d995b7fe996277e6a24089
Tomoyuki Kawano <kawatomo@city.co.jp>
2019-08-29 23:14:02 +0900

    曖昧なキャラ名から正確なキャラ名を引けるようにした.

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,6 +5,5 @@ import * as sut from "../src/index";
 const s3 = new AWS.S3();
 
 (async () => {
-    const res = await sut.getTotalPlayState();
-    console.log(res.body);
+    console.log(sut.getCorrectCharacterName("teh locker"));
 })();
